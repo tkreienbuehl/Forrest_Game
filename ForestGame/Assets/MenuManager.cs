9083f06a1d3764650bcb2ad56ead7fe14b7cacc2
Ruben de Groot <ruben_degroot@hotmail.com>
2017-08-17 11:28:43 +0200

    Added base for campaign interface

---
+++ ForestGame/Assets/MenuManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour {
+
+	public void SwitchToGameSceneCampaignConservatives()
+	{
+		SceneManager.LoadScene (1);
+	}
+
+	public void SwitchToGameSceneCampaignDemocrats()
+	{
+		SceneManager.LoadScene (2);
+	}
+
+	public void SwitchToGameSceneCampaignEnvironmentalist()
+	{
+		SceneManager.LoadScene (3);
+	}
+}
