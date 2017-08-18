06dff1bc8009fede45606a9d97a81e83ceeea07d
Darryl van Denzel <darryl.van.denzel@hva.nl>
2017-08-18 12:39:21 +0200

    map update

---
+++ ForestGame/Assets/fill.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fill : MonoBehaviour {
+
+    public GameObject go;
+    public Vector3[] pos = new Vector3[transform.childCount]; 
+
+	// Use this for initialization
+	void Start () {
+		foreach(Vector3 in pos)
+        {
+
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
