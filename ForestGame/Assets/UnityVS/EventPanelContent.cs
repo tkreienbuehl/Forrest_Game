f81a73b48bdea82b2a915ecae5d67a4fc866409c
Siem van der Kluft <svdkluft@gmail.com>
2017-08-20 15:38:21 +0200

    Added the event content fill and the button press response

---
+++ ForestGame/Assets/UnityVS/EventPanelContent.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Internal.Execution;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EventPanelContent : MonoBehaviour
+{
+    private IEventPanelObserver iEventPanelObserver;
+
+    private GameObject canvas;
+    private int eventID;
+    private short factionID;
+    private Text eventText;
+
+    private GameObject eventPanel;
+
+    public void SetEvent(IEvent iEvent)
+    {
+        canvas = GameObject.Find("PanelCanvas");
+        eventPanel = canvas.transform.Find("EventPanel").gameObject;
+        eventText = eventPanel.transform.Find("Panel").transform.Find("EventDescription").GetComponent<Text>();
+
+        eventText.text = iEvent.getEventText();
+        eventID = iEvent.getEventID();
+        factionID = iEvent.getFactionID();
+    }
+
+    public void AcceptEvent()
+    {
+        iEventPanelObserver.setEventCommited();
+    }
+
+    public void RegisterObserver(IEventPanelObserver iEventPanelObserver)
+    {
+        this.iEventPanelObserver = iEventPanelObserver;
+    }
+}
