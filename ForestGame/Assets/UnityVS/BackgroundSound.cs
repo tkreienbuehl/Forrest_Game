c5378955eff29e72c5ad9d4056854428f7cac6bf
Ruben de Groot <ruben_degroot@hotmail.com>
2017-08-21 11:11:06 +0200

    Made the background sound a Singleton

---
+++ ForestGame/Assets/UnityVS/BackgroundSound.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundSound : MonoBehaviour {
+
+	public AudioClip background_sound;
+
+	private static BackgroundSound instance = null;
+
+	public static BackgroundSound Instance {
+		get { return instance; }
+	}
+
+	void Awake() {
+		if (instance != null && instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			instance = this;
+		}
+		DontDestroyOnLoad(this.gameObject);
+	}
+
+}
