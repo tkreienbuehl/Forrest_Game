14006bba7f7720720edf86f6012eba4a0a63f09b
Sebastiaan Bosschieter <hallosebastiaan@hotmail.com>
2017-08-22 00:52:59 +0200

    added the decissions to the database, tested the queries, added income cost and cost yearly to get function database

--- ForestGame/Assets/UnityVS/Influences.cs
+++ ForestGame/Assets/UnityVS/Influences.cs
@@ -4,10 +4,18 @@
     private short environmentalInfluence;
     private short touristicInfluence;
 
+    //added new part.
+    private decimal Cost;
+    private decimal Cost_Yearly;
+    private decimal Income;
+
     public Influences() {
-        industrialInfluence = 0;
-        environmentalInfluence = 0;
-        touristicInfluence = 0;
+        industrialInfluence     = 0;
+        environmentalInfluence  = 0;
+        touristicInfluence      = 0;
+        Cost                    = 0;
+        Cost_Yearly             = 0;
+        Income                  = 0;
     }
 
     public void setIndustrialInfluence(short influence) {
@@ -32,6 +40,30 @@
 
     public short getTouristicalInfluence() {
         return touristicInfluence;
+    }
+
+    public void setCostInfluence(decimal influence) {
+        Cost = influence;
+    }
+
+    public void setCostYearlyInfluence(decimal influence) {
+        Cost_Yearly = influence;
+    }
+
+    public void setIncomeInfluence(decimal influence) {
+        Income = influence;
+    }
+
+    public decimal getCostInfluence() {
+        return Cost;
+    }
+
+    public decimal getCostYearlyInfluence() {
+        return Cost_Yearly;
+    }
+
+    public decimal getIncomeInfluence() {
+        return Income;
     }
 
 }
