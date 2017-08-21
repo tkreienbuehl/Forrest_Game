92a69d493462b0b6478d1e3da79e6f997cf33d2d
Darryl van Denzel <darryl.van.denzel@hva.nl>
2017-08-21 11:16:08 +0200

    Adds object grid

--- ForestGame/Assets/fill.cs
+++ ForestGame/Assets/fill.cs
@@ -5,13 +5,21 @@ using UnityEngine;
 public class fill : MonoBehaviour {
 
     public GameObject go;
-    public Vector3[] pos = new Vector3[transform.childCount]; 
+    public Vector3[] pos;
+    public GameObject treeFab;
 
-	// Use this for initialization
-	void Start () {
-		foreach(Vector3 in pos)
+    // Use this for initialization
+    void Start () {
+        Vector3[] pos = new Vector3[transform.childCount];
+        for(int i = 0; i < pos.Length; i++)
         {
+            pos[i] = go.transform.GetChild(i).position;
+        }
 
+        foreach (Vector3 positions in pos)
+        {
+            GameObject gameObject = Instantiate(treeFab, positions, new Quaternion(0, 0, 0, 1));
+             
         }
 	}
 	
