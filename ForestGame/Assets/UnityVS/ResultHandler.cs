b4194e85bf34e1bf843ae5cfb2388a2f61ad4085
Khoa Nguyen <khoa.nguyen2@hva.nl>
2017-08-21 14:13:00 +0200

    Influences bar added

---
+++ ForestGame/Assets/UnityVS/ResultHandler.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ResultHandler : MonoBehaviour
+{
+    public Image environmental;
+    public Image industry;
+    public Image tourist;
+
+    Influences influences;
+
+    void Start() {
+        
+    }
+
+    void Update()
+    {
+        // TODO calculate the amount of influences
+        environmental.fillAmount = 1f;
+        industry.fillAmount = 0.2f;
+        tourist.fillAmount = 0.7f;
+    }
+
+}
