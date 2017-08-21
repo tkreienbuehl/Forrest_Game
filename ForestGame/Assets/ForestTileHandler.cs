671c317a50330fd5faa355c66f0b6877031dea76
Tom <tom.bosman2@hva.nl>
2017-08-21 14:41:36 +0200

    Selection added

---
+++ ForestGame/Assets/ForestTileHandler.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForestTileHandler : MonoBehaviour {
+
+	public Color GlowColor;
+	public float LerpFactor = 10;
+
+	public Renderer[] Renderers
+	{
+		get;
+		private set;
+	}
+
+	public Color CurrentColor
+	{
+		get { return _currentColor; }
+	}
+
+	private List<Material> _materials = new List<Material>();
+	private Color _currentColor;
+	private Color _targetColor;
+
+	void Start()
+	{
+		Renderers = GetComponentsInChildren<Renderer>();
+
+		foreach (var renderer in Renderers)
+		{	
+			_materials.AddRange(renderer.materials);
+		}
+	}
+
+	private void OnMouseEnter()
+	{
+		_targetColor = GlowColor;
+		enabled = true;
+	}
+
+	private void OnMouseExit()
+	{
+        _targetColor = Color.clear;
+		enabled = true;
+	}
+
+	/// <summary>
+	/// Loop over all cached materials and update their color, disable self if we reach our target color.
+	/// </summary>
+	private void Update()
+	{
+		_currentColor = Color.Lerp(_currentColor, _targetColor, Time.deltaTime * LerpFactor);
+
+		for (int i = 0; i < _materials.Count; i++)
+		{
+			_materials[i].SetColor("_Color", _currentColor);
+		}
+
+		if (_currentColor.Equals(_targetColor))
+		{
+			enabled = false;
+		}
+	}
+}
