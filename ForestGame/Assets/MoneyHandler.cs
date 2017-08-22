08281a0b9d72d397a501fe49f0d3374337dae7d6
Siem van der Kluft <svdkluft@gmail.com>
2017-08-22 15:42:48 +0200

    Added money UI

---
+++ ForestGame/Assets/MoneyHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyHandler : MonoBehaviour
+{
+
+    public Text moneyText;
+    public Text yearlyCostText;
+
+    private float money;
+    private float yearlyCost;
+
+    void Start()
+    {
+        money = 20000;
+        yearlyCost = 10000;
+        moneyText.text = "Money: " + money;
+        yearlyCostText.text = "Yearly cost: " + yearlyCost;
+    }
+
+    public void ChangeMoneyAmount(float change)
+    {
+        money += change;
+        moneyText.text = "Money: " + money;
+    }
+
+    public void ChangeYearlyCostAmount(float change)
+    {
+        yearlyCost += change;
+        yearlyCostText.text = "Yearly cost: " + yearlyCost;
+    }
+}
