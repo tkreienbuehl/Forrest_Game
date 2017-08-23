89659517cfa4a22a21190551195be0365dd8b9a2
Sebastiaan Bosschieter <hallosebastiaan@hotmail.com>
2017-08-23 11:31:50 +0200

    finished version of the Party class

--- ForestGame/Assets/UnityVS/Party.cs
+++ ForestGame/Assets/UnityVS/Party.cs
@@ -2,16 +2,29 @@
 {
     public static int partyID;
     public static int sloganID;
+    public static float industry;
+    public static float tourism;
+    public static float nature;
 
     public void getBasicValue() {
 
         if (partyID == 1) { //industry
 
-            
+            industry    = 60;
+            tourism     = 50;
+            nature      = 40;
 
         } else if () { //Tourism
 
+            industry = 40;
+            tourism = 60;
+            nature = 50;
+
         } else if () { //Enviroment
+
+            industry = 40;
+            tourism = 50;
+            nature = 60;
 
         }
     }
